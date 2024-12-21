6d7778908dcd97fa78eacf367cd5d5ac335a9ca1
mrsahiljaiswal <sahiljaiswal757@gmail.com>
2024-12-21 18:51:33 +0530

    added authentication

--- server/firebase-admin.js
+++ server/firebase-admin.js
@@ -2,13 +2,11 @@
 const admin = require('firebase-admin');
 
 // Path to your service account key (downloaded JSON file)
-const serviceAccount = require('./path/to/serviceAccountKey.json'); // Correct path to your JSON file
+const serviceAccount = require('./path/to/serviceAccountKey.json'); 
 
-// Initialize Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  // Optional: If using Firebase Realtime Database
-  // databaseURL: 'https://your-database-name.firebaseio.com'
+ 
 });
 
 module.exports = admin;
